685a8705246ea6833f1111634cc0e16718c46783
Alon Albert <aalbert@google.com>
2023-12-02 09:24:38 -0800

    Cosmetics

--- app/src/test/java/com/alonalbert/karoogeardetector/module/SpeedInfo.kt
+++ app/src/test/java/com/alonalbert/karoogeardetector/module/SpeedInfo.kt
@@ -2,7 +2,12 @@ package com.alonalbert.karoogeardetector.module
 
 import io.hammerhead.sdk.v0.datatype.Dependency
 
-internal class SpeedInfo(private val rpm: Int, private val speed: Double, val front: Int, val rear: Int) {
+internal class SpeedInfo(
+  private val rpm: Int,
+  private val speed: Double,
+  val front: Int,
+  val rear: Int
+) {
   fun toDependencies() =
     mapOf(Dependency.SPEED to speed.kphToMps(), Dependency.CADENCE to rpm.toDouble())
 }
